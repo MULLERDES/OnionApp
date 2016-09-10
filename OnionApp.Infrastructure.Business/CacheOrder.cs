39b445de66cd412bd5c60422c7b096947d566b69
MULLERDES <amelnikd@gmail.com>
2016-09-10 22:19:56 +0300

    initial

---
+++ OnionApp.Infrastructure.Business/CacheOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnionApp.Domain.Core;
+using OnionApp.Services.Interfaces;
+
+namespace OnionApp.Infrastructure.Business
+{
+    public class CacheOrder :IOrder
+    {
+        public void MakeOrder(Book book)
+        {
+            // код покупки книги при оплате наличностью
+        }
+    }
+}
---
+++ OnionApp.Infrastructure.Business/CreditOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnionApp.Domain.Core;
+using OnionApp.Services.Interfaces;
+
+namespace OnionApp.Infrastructure.Business
+{
+    public class CreditOrder:IOrder
+    {
+        public void MakeOrder(Book book)
+        {
+            //Buy in credit
+            //throw new NotImplementedException();
+        }
+    }
+}
